33356bf095c37bd1c3810c7db1a512ea6b4f67f7
Yong <cucrn35@gmail.com>
2021-12-09 00:53:25 +0900

    http2, undertow, spring security 로 첫화면 띄우기 성공!

---
+++ src/main/java/com/project/blog/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.project.blog.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    // @Autowired
+    // DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/account/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    
+
+}
